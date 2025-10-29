c8351d8360e37a4463b0b52e62bf90bfb61fbf99
jonathan choi <jchoi4497@gmail.com>
2025-10-28 19:48:54 -0700

    styling

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -7,6 +7,10 @@
     <div class="bg-white p-6 max-w-xl mx-auto rounded-2xl shadow-lg mt-8">
       <h1 class="text-3xl font-bold mb-4 text-sky-600">Contact Me</h1>
       <p class="text-gray-700 mb-2">Email: jchoi4497@Gmail.com</p>
+      <p class="text-gray-700 mb-2">Github: https://github.com/jchoi4497</p>
+      <p class="text-gray-700 mb-2">
+        LinkedIn: https://www.linkedin.com/in/jonathan-choi-89026b198/
+      </p>
     </div>
   </div>
 </template>
